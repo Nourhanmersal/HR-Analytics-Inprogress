2afc955d4a246d9b991c1e719c31faba6aa45f85
Nourhan Mersal <nourhanmersal@hotmail.com>
2026-02-22 13:28:21 +0100

    Add data cleaning queries for HR dataset
    Added queries for data cleaning including checks for nulls, duplicates, and data typing.

--- SQL queries.sql
+++ SQL queries.sql
@@ -1,1 +1,19 @@
+Data_Cleaning
+--checking for nulls (the below query has been repeated on all the other coloms to check for nulls and all returned with no data to display)
+SELECT satisfaction_level
+FROM `temp-table-ex-485819.HRDataset.HRdataset`
+where satisfaction_level is null
 
+-- Checking for duplicates (since we dont have a unique value, we cant say that the dulicates are inaccurate).
+SELECT distinct satisfaction_level
+FROM `temp-table-ex-485819.HRDataset.HRdataset`
+
+--Data Typing (Ensure left, Work_accident, and promotion_last_5years are treated as boolean or has only two outcomes)
+SELECT distinct Work_accident,
+FROM `temp-table-ex-485819.HRDataset.HRdataset`
+
+SELECT distinct promotion_last_5years,
+FROM `temp-table-ex-485819.HRDataset.HRdataset`
+
+SELECT distinct `left`
+FROM `temp-table-ex-485819.HRDataset.HRdataset`
